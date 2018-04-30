7365f0d6fcba264d86798831442db56806add2b4
Kimberly <ksereduck@mozilla.com>
2018-04-30 15:14:52 -0600

    replacing single quotes with double quotes for consistency

--- tests/conftest.py
+++ tests/conftest.py
@@ -12,10 +12,10 @@ if find_dotenv():
 
 @pytest.fixture(scope="module")
 def secure_user_one():
-    bug_host = os.getenv('BUGZILLA_HOST')
-    bug_key =  os.getenv('BUGZILLA_API_KEY_1')
-    phab_host = os.getenv('PHABRICATOR_API_URL')
-    phab_key = os.getenv('PHABRICATOR_API_KEY_1')
+    bug_host = os.getenv("BUGZILLA_HOST")
+    bug_key =  os.getenv("BUGZILLA_API_KEY_1")
+    phab_host = os.getenv("PHABRICATOR_API_URL")
+    phab_key = os.getenv("PHABRICATOR_API_KEY_1")
 
     user = User(bug_host, bug_key, phab_host, phab_key)
     return user
