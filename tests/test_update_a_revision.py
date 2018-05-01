4f4a88999a9e24719c22080d9300b21825c310a4
Kimberly <ksereduck@mozilla.com>
2018-05-01 15:13:57 -0600

    added more checks to update revision test

--- tests/test_update_a_revision.py
+++ tests/test_update_a_revision.py
@@ -13,4 +13,18 @@ def test_update_a_revision(secure_user_one):
     updated_revision = secure_user_one.update_revision(bug_id, diff, revision)
     print(updated_revision)
     assert updated_revision["object"]["phid"] == revision["object"]["phid"]
-    assert update_revision["object"]["id"] == revision["object"]["id"]
+    assert updated_revision["object"]["id"] == revision["object"]["id"]
+
+    """
+    Now read back details of the revision and make sure values are as
+    we expect them to be
+    """
+    revision_id = updated_revision["object"]["id"]
+    revision_search = secure_user_one.search_for_revision(revision_id)
+    assert revision_search["id"] == revision_id
+    assert revision_search["fields"]["title"] == "Commit message"
+    assert revision_search["fields"]["summary"] == "Summary"
+    assert revision_search["fields"]["testPlan"] == "QA update a revision"
+    assert revision_search["fields"]["bugzilla.bug-id"] == "Commit message"
+    assert revision_search["fields"]["title"] == "Commit message"
+    assert revision_search["fields"]["title"] == "Commit message"
